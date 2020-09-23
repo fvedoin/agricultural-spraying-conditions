3351b6fde9b9e7c86161b99e12e3eb7dd0ef31ac
Fernando <fernando.vgarcia@hotmail.com>
2020-09-23 14:28:32 -0300

    MQTT and WiFi communication added.

--- device_controller/device_controller.ino
+++ device_controller/device_controller.ino
@@ -1,4 +1,22 @@
 #include "DHT.h"
+#include <iostream>
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
+
+//Wifi configs
+#define WIFI_SSID "At"
+#define WIFI_PASSWORD "felipe10"
+
+//MQTT configs
+#define MQTT_HOST IPAddress(192, 168, 43, 39)
+#define MQTT_PORT 1883
+
+//topics
+#define MQTT_PUB_TEMP "esp/dht/temperature"
+#define MQTT_PUB_HUM "esp/dht/humidity"
+#define MQTT_PUB_WIND "esp/wind"
+#define MQTT_PUB_FLOW "esp/flow"
 
 #define DHTPIN 12     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
@@ -9,8 +27,93 @@
 #define increase D8
 
 int flow = 0;
+char* rain;
 
 DHT dht(DHTPIN, DHTTYPE);
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+  uint16_t packetIdSub = mqttClient.subscribe("esp/rain", 1);
+  Serial.print("Subscribing at QoS 1, packetId: ");
+  Serial.println(packetIdSub);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+  rain = payload;
+}
 
 void setup() {
   pinMode(redLed, OUTPUT);
@@ -18,15 +121,28 @@ void setup() {
   pinMode(yellowLed, OUTPUT);
   pinMode(decrease, INPUT);
   pinMode(increase, INPUT);
-  Serial.begin(9600); 
+  Serial.begin(115200); 
   dht.begin();
+
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  
+  connectToWifi();
 }
 
 void loop() {
   delay(2000);
 
   //Read wind speed
-  float w = analogRead(A0);
+  float w = analogRead(A0)/50;
   // Read humidity
   float h = dht.readHumidity();
   // Read temperature as Celsius
@@ -47,26 +163,22 @@ void loop() {
   }
   int cWind, cHumidity, cTemperature;
   
-  if(t < 10 || t > 35) {
-    cTemperature = 3;
-  } else if((t >= 10 && t < 20) || (t > 30 && t <= 35)){
-    cTemperature = 2;
-  } else if(t >= 20 && t <= 30){
+  if(t > 15 && t < 32) {
     cTemperature = 1;
+  }else{
+    cTemperature = 2;
   }
 
-  if(h < 60 || h > 95) {
-    cHumidity = 3;
-  } else if((h >= 60 && h < 70) || (h > 90 && h <= 95)){
-    cHumidity = 2;
-  } else if(h >= 70 && h <= 90){
+  if(h > 55 && h < 95) {
     cHumidity = 1;
+  } else {
+    cHumidity = 2;
   }
 
-  if(w <= 10) {
+  if(w > 2 && w < 10) {
     cWind = 1;
   } else {
-    cWind = 3;
+    cWind = 2;
   }
 
   float condition = (cTemperature + cWind + cHumidity) / 3.0;
@@ -75,7 +187,7 @@ void loop() {
     digitalWrite(blueLed, HIGH);
     digitalWrite(redLed, LOW);
     digitalWrite(yellowLed, LOW);
-  } else if (condition > 1 && condition <= 2){
+  } else if (condition > 1 && condition < 1.34){
     digitalWrite(blueLed, LOW);
     digitalWrite(redLed, LOW);
     digitalWrite(yellowLed, HIGH);
@@ -95,10 +207,28 @@ void loop() {
   Serial.print("Wind: ");
   Serial.print(w);
   Serial.println(" Km/h ");
-
+  Serial.println("------------------------------------");
+  Serial.println(rain);
+  Serial.println("------------------------------------");
+  
+  uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(t).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_TEMP, packetIdPub1);
+    Serial.printf("Message: %.2f \n", t);
+    
+  uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(h).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+    Serial.printf("Message: %.2f \n", h);
+    
+  uint16_t packetIdPub3 = mqttClient.publish(MQTT_PUB_WIND, 1, true, String(w).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_WIND, packetIdPub3);
+    Serial.printf("Message: %.2f \n", w);
+  
   if(flow > 0){
     Serial.print("*Spraying* \t Flow: ");
     Serial.print(flow);
     Serial.println(" L/s");
+    uint16_t packetIdPub4 = mqttClient.publish(MQTT_PUB_FLOW, 1, true, String(flow).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_FLOW, packetIdPub4);
+    Serial.printf("Message: %.2f \n", flow);
   }
 }
